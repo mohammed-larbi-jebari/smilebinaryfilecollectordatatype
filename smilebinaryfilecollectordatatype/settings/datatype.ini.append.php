f571e8831b2327260630a66b67a36c2b1c97ca31
Mohammed l'arbi Jebari <arbito82@gmail.com>
2014-08-05 16:25:49 +0100

    delete parent folder

--- smilebinaryfilecollectordatatype/settings/datatype.ini.append.php
+++
@@ -1,9 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[EditSettings]
-GroupedInput[]=smilebinaryfile
-
-[ViewSettings]
-CustomActionMap[ActionDeleteSmileBinaryFile]=smilebinaryfile/deletebinary
-
-*/ ?>
--- smilebinaryfilecollectordatatype/settings/site.ini.append.php
+++
@@ -1,9 +0,0 @@
-<?php /* #?ini charset="utf-8"?
-
-[TemplateSettings]
-ExtensionAutoloadPath[]=smilebinaryfilecollectordatatype
-
-[RegionalSettings]
-TranslationExtensions[]=smilebinaryfilecollectordatatype
-
-*/ ?>
